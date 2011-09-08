8bcd9d4067fb624090cadfc996740e364547997f
rob <rob@little.(none)>
2011-09-08 14:32:05 +0200

    -implemented many-to-many between user-location

--- src/main/scala/lards/model/dto/User.scala
+++ src/main/scala/lards/model/dto/User.scala
@@ -11,7 +11,7 @@ class User(val pid: java.lang.Long = -1,
   extends Dto(pid) {
 
   @BeanProperty var role: Role = new Role
-  @BeanProperty var location: java.util.Set[Location] = new java.util.HashSet[Location]
+  @BeanProperty var location = new java.util.HashSet[Location]
   
   override def is_same_type(other: lards.model.dto.Dto): Boolean = 
     other.isInstanceOf[User]
